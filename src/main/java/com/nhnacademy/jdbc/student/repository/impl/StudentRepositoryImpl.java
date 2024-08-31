524d102c02aa00b7fe1f3fd4ed7c7f363b8271e4
YONGTAEK LEE <itaekit@YONGTAEKui-MacBookPro.local>
2024-08-31 10:28:05 +0900

    Solved

--- src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
+++ src/main/java/com/nhnacademy/jdbc/student/repository/impl/StudentRepositoryImpl.java
@@ -123,13 +123,65 @@ public class StudentRepositoryImpl implements StudentRepository {
     @Override
     public long totalCount(Connection connection) {
         //todo#4 totalCount 구현
+        String sql = "SELECT COUNT(*) FROM jdbc_students";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return 0l;
     }
 
     @Override
     public Page<Student> findAll(Connection connection, int page, int pageSize) {
         //todo#5 페이징 처리 구현
-        return null;
-    }
+        //page : 내가 클릭한 페이지 번호
+        // pagesize : 한 페이지에서 노출되는 컨텐츠 수
+        // 즉 내가 누른 페이지에서 노출되어야 할 컨텐츠를 보여줘야 함
 
+        // 전체 행 수
+        long totalRows = totalCount(connection);
+
+        // 전체 페이지 수
+        long totalPages = (long)Math.ceil((double)totalRows / pageSize);
+
+        List<Student> list = new ArrayList<>();
+
+
+        /*
+                총 32개의 글
+                pageSize : 10
+                totalPages = ceil(32/10) => 4 페이지로 제공
+
+                1번 페이지 : 1~10
+                2번 페이지 : 11~20
+                3번 페이지 : 21~30
+                4번 페이지 : 31~32
+         */
+        int start = (page - 1) * pageSize;
+        int cnt = pageSize;
+
+        String sql = "SELECT * FROM jdbc_students LIMIT ?, ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setInt(1, start);
+            preparedStatement.setInt(2, cnt);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                list.add(new Student(
+                                    rs.getString("id"),
+                                    rs.getString("name"),
+                                    Student.GENDER.valueOf(rs.getString("gender")),
+                                    rs.getInt("age")
+                        ));
+            }
+
+            return new Page<Student>(list, totalRows);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
